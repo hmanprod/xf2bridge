1d1fc39d52ad683753b64afe6bfa1291447c7ac9
RABOTOVAO Hery <hmanprod@gmail.com>
2019-12-09 17:52:39 +0300

    Fix cause XF already bootstrap on Laravel

--- src/XF2Bridge/XF2Bridge.php
+++ src/XF2Bridge/XF2Bridge.php
@@ -52,9 +52,10 @@ class XF2Bridge
     {
         $path = $this->directoryPath . '/src/XF.php';
         if( file_exists($path) && is_readable($path) && require_once($path)) {
-            \XF::start($this->directoryPath);
-            $app = \XF::setupApp('XF\Pub\App');
-            $app->start();
+            //\XF::start($this->directoryPath);
+            //$app = \XF::setupApp('XF\Pub\App');
+            //$app->start();
+		$app = \XF::app();
         } else
             throw new \Exception('Could not load XenForo check path: ' . $path);
     }

3b8b026e3ef4f1b0af226a46485f24b84d6943fb
RABOTOVAO Hery <hmanprod@gmail.com>
2020-01-13 22:03:05 +0300

    Update config file

--- src/config/xf2bridge.php
+++ src/config/xf2bridge.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 
 return array(
-    'xenforo_directory_path' => 'FULL PATH HERE',
-    'xenforo_base_url_path'  => '/', //Default '/',
+    'xenforo_directory_path' => env('XENFORO_DIRECTORY_PATH', '/'),
+    'xenforo_base_url_path'  => env('XENFORO_URL_PATH', '/'),
     'use_xenforo_auth'       => true,
+    'xenforo_api_key'        => env('XENFORO_API_KEY')
 );
+
